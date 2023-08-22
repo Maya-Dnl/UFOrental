b293d6a861f014012c0ab12d3a7f8c9a857899fe
Jessica Zgheib <jessicazgheib22@outlook.com>
2023-08-22 16:56:00 +0200

    Removed User model

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  has_many :flyingsaucers
-  has_many :bookings
-end
